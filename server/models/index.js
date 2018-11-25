6ece106e65b3e9cfde9db2671be3584b21ec1bf0
Henry <pienry3g@gmail.com>
2018-11-25 12:58:34 +0100

    Update .env and models/index.js
    - Use the correct environmental variable names in server/models/index.js
    - set all db connection strings in .env to use elepahnt sql except MY_POSTICO_URL env variable

--- server/models/index.js
+++ server/models/index.js
@@ -2,25 +2,26 @@ import fs from 'fs';
 import path from 'path';
 import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
+import config from '../config/config';
 
 const basename = path.basename(__filename);
-const { NODE_ENV } = process.env;
 const db = {};
 
 dotenv.config();
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 let DB_URL;
 switch (NODE_ENV) {
   case 'development':
-    DB_URL = process.env.LOCAL_DATABASE_URL;
+    DB_URL = process.env.DEV_DB_URL;
     break;
 
   case 'production':
-    DB_URL = process.env.REMOTE_DATABASE_URL;
+    DB_URL = process.env.PROD_DB_URL;
     break;
 
   case 'test':
-    DB_URL = process.env.TEST_DATABASE_URL;
+    DB_URL = process.env.TEST_DB_URL;
     break;
 
   default:
@@ -28,7 +29,9 @@ switch (NODE_ENV) {
     break;
 }
 
-const sequelize = new Sequelize(DB_URL, DB_URL);
+// console.log(process.env[config[NODE_ENV].use_env_variable]);
+
+const sequelize = new Sequelize(DB_URL, config[NODE_ENV]);
 
 fs.readdirSync(__dirname)
   .filter(file => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
