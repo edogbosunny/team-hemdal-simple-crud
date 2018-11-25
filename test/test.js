a16591e8d2d7c3e07e384445794db9dc0f1bba68
Sunny Edogbo <sunny.edogbo@andela.com>
2018-11-25 23:09:21 +0100

    implementSequelizeModel

--- test/test.js
+++ test/test.js
@@ -1,24 +1,24 @@
-// import chai, { expect } from 'chai';
-// import chaiHttp from 'chai-http';
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
 
-// import app from '../index';
+import app from '../index';
 
-// chai.use(chaiHttp);
+chai.use(chaiHttp);
 
-// describe('Test for creating a new Todo', () => {
-//   it('should return 201 for a successfully creating a Todo', (done) => {
-//     const data = {
-//       title: 'Test Title',
-//     };
-//     chai
-//       .request(app)
-//       .post('/api/v1/todo')
-//       .send(data)
-//       .end((err, res) => {
-//         console.log(res.body);
-//         expect(res).to.have.status(201);
-//         expect(res.body).to.be.an('object');
-//         done();
-//       });
-//   });
-// });
+describe('Test for creating a new Todo', () => {
+  it('should return 201 for a successfully creating a Todo', (done) => {
+    const data = {
+      title: 'Test Title',
+    };
+    chai
+      .request(app)
+      .post('/api/v1/todo')
+      .send(data)
+      .end((err, res) => {
+        console.log(res.body);
+        expect(res).to.have.status(201);
+        expect(res.body).to.be.an('object');
+        done();
+      });
+  });
+});
